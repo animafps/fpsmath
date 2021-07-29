c9f05126f6739cf990532a2f00095d5701b44d8c
Anima <lukecsnz@gmail.com>
2021-07-30 10:48:49 +1200

    docs(games): sorted the game command alphabetically

--- src/array.ts
+++ src/array.ts
@@ -7,58 +7,26 @@ interface Array {
   };
 }
 const array: Array = {
+  "3D Aim Trainer": {
+    yaw: 0.0066,
+    aliases: ["3daimtrainer", "3dat"],
+    afovt: "16MF9",
+  },
   Aimgods: { yaw: 0.0023331, aliases: ["aimgods"] },
-  Source: {
-    yaw: 0.022,
-    aliases: ["source"],
-    afovt: "4ML3",
-  },
-  "Counter Strike": {
-    yaw: 0.022,
-    aliases: ["cs", "cs:go", "csgo", "counter-strike"],
-    afovt: "4ML3",
-  },
-  Quake: { yaw: 0.022, aliases: ["quake"], afovt: "4ML3" },
-  "Quake Champions": {
-    aliases: ["qc", "quake-champions"],
-    afovt: "16ML9",
-    yaw: 0.022,
-  },
   "Apex Legends": {
     yaw: 0.022,
     aliases: ["apex-legends", "apex"],
     afovt: "4ML3",
   },
-  Valorant: { yaw: 0.07, aliases: ["valorant", "val"] },
-  Overwatch: {
-    yaw: 0.0066,
-    aliases: ["overwatch", "ow"],
-    afovt: "16MF9",
-  },
-  Fortnite: {
-    yaw: 0.005555,
-    aliases: ["fortnite", "fn"],
-    afovt: "16ML9",
-  },
-  "Fortnite config": {
-    yaw: 2.222,
-    aliases: ["fn-config", "fortnite-config"],
-    afovt: "16ML9",
-  },
-  Diabotical: {
-    yaw: 0.022,
-    aliases: ["diabotical", "dbt"],
-    afovt: "vML",
-  },
-  "Rainbow Six: Siege": {
-    yaw: 0.005729577951308232,
-    aliases: ["r6", "rainbow6", "r6s", "siege"],
-    afovt: "vML",
-  },
   "Call of Duty": {
     yaw: 0.0066,
     aliases: ["cod", "callofduty", "call-of-duty", "warzone", "wz"],
     afovt: "hML",
+  },
+  "Counter Strike": {
+    yaw: 0.022,
+    aliases: ["cs", "cs:go", "csgo", "counter-strike"],
+    afovt: "4ML3",
   },
   Battlefield: {
     yaw: 0.6771319397,
@@ -70,10 +38,25 @@ const array: Array = {
     aliases: ["destiny", "d2", "destiny2"],
     afovt: "16ML9",
   },
-  Reflex: {
-    yaw: 0.005729577951308232087679815481411,
-    aliases: ["reflex", "reflex-arena"],
-    afovt: "4ML3",
+  Diabotical: {
+    yaw: 0.022,
+    aliases: ["diabotical", "dbt"],
+    afovt: "vML",
+  },
+  Fortnite: {
+    yaw: 0.005555,
+    aliases: ["fortnite", "fn"],
+    afovt: "16ML9",
+  },
+  "Fortnite config": {
+    yaw: 2.222,
+    aliases: ["fn-config", "fortnite-config"],
+    afovt: "16ML9",
+  },
+  "Halo: Master Chief Collection": {
+    yaw: 0.022222222222222223,
+    aliases: ["halo", "halo-mcc"],
+    afovt: "hML",
   },
   Krunker: {
     yaw: 0.13750954927425516,
@@ -85,6 +68,11 @@ const array: Array = {
     aliases: ["mc", "minecraft"],
     afovt: "hML",
   },
+  Overwatch: {
+    yaw: 0.0066,
+    aliases: ["overwatch", "ow"],
+    afovt: "16MF9",
+  },
   Palidins: {
     aliases: ["palidins"],
     afovt: "hML",
@@ -93,6 +81,27 @@ const array: Array = {
     aliases: ["pubg"],
     yaw: 2.49975,
     afovt: "16ML9",
+  },
+  Quake: { yaw: 0.022, aliases: ["quake"], afovt: "4ML3" },
+  "Quake Champions": {
+    aliases: ["qc", "quake-champions"],
+    afovt: "16ML9",
+    yaw: 0.022,
+  },
+  "Rainbow Six: Siege": {
+    yaw: 0.005729577951308232,
+    aliases: ["r6", "rainbow6", "r6s", "siege"],
+    afovt: "vML",
+  },
+  Reflex: {
+    yaw: 0.005729577951308232087679815481411,
+    aliases: ["reflex", "reflex-arena"],
+    afovt: "4ML3",
+  },
+  Source: {
+    yaw: 0.022,
+    aliases: ["source"],
+    afovt: "4ML3",
   },
   "Totally Accurate Battlegrounds": {
     aliases: ["tabg"],
@@ -103,16 +112,7 @@ const array: Array = {
     aliases: ["ue4", "unreal", "unreal-engine"],
     yaw: 0.07,
   },
-  "Halo: Master Chief Collection": {
-    yaw: 0.022222222222222223,
-    aliases: ["halo", "halo-mcc"],
-    afovt: "hML",
-  },
-  "3D Aim Trainer": {
-    yaw: 0.0066,
-    aliases: ["3daimtrainer", "3dat"],
-    afovt: "16MF9",
-  },
+  Valorant: { yaw: 0.07, aliases: ["valorant", "val"] },
 };
 
 export function getObject(
